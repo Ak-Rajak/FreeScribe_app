52f37f67ffde26c29bb43430e328be93ca121250
Ak-Rajak <atulkumar.coderguys@gmail.com>
2025-02-04 12:43:29 +0530

    started working on the hero page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,22 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div className="text-green-400">Hello</div>
-    </>
+    <div className="flex flex-col p-4">
+      <section className="min-h-screen flex flex-col">
+        <header>
+          <h1>Free<span className='text-blue-400'>Scribe</span></h1>
+        </header>
+        <main className='flex-1'>
+
+        </main>
+        <footer>
+
+        </footer>
+      </section>
+    </div>
   )
 }
 
